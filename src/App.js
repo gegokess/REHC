4bd5a402f8f592d9461a1d3c9350ffe70d19683e
themkde <grex1@gmx.de>
2021-05-23 22:44:16 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useContext } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import "./App.css";
+import { Grid, GridItem, Box } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
 import Results from "./components/Results";
+import Selection from "./components/Selection";
 import { CalculationProvider } from "./context/CalculationContext";
 import HomeInstallation from "./components/HomeInstallation";
 
@@ -25,8 +27,14 @@ function App() {
 		<CalculationProvider>
 			<div className="App">
 				<ChakraProvider>
-					<Container>
-						<HomeInstallation />
+					<Container maxW="container.lg">
+						<Grid w="100%" templateColumns="repeat(2, 1fr)" gap={6}>
+							<Box w="100%" bg="blue.500" />
+
+							<Box w="100%" bg="blue.500">
+								<Selection name="HeatPump" />
+							</Box>
+						</Grid>
 					</Container>
 				</ChakraProvider>
 			</div>
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,50 +1,6 @@
 import React, { setState } from "react";
 import HomeInstallation from "./HomeInstallation";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Divider from "@material-ui/core/Divider";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Switch from "@material-ui/core/Switch";
-import Grid from "@material-ui/core/Grid";
-import Slider from "@material-ui/core/Slider";
 
 export default function Selection({ items, useSelection, name }) {
-	return (
-		<div className="my-4">
-			<Card>
-				<Grid container spacing={1} display="inline">
-					<Grid item xs={2}>
-						<Switch
-							checked={useSelection.value}
-							onChange={useSelection.onChange}
-							name="checked"
-							color="primary"
-						/>
-					</Grid>
-					<Divider orientation="vertical" flexItem />
-					<Grid
-						item
-						xs={9}
-						alignItems="center"
-						justify="center"
-						direction="column"
-					>
-						<Typography variant="h6">{name}</Typography>
-					</Grid>
-				</Grid>
-
-				<Divider />
-				<CardContent>
-					{items.map((item) => (
-						<HomeInstallation
-							disabled={!useSelection.value}
-							item={item}
-						/>
-					))}
-				</CardContent>
-			</Card>
-		</div>
-	);
+	return <div className="my-4">{name}</div>;
 }
