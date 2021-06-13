0130f67988e79af1a7d9ccc5caece98487db6224
themkde <grex1@gmx.de>
2021-06-13 11:15:00 +0200

    updates

--- src/components/daySelection.js
+++ src/components/daySelection.js
@@ -4,28 +4,30 @@ import DaySelectionElement from "./daySelectionElement";
 import { CalculationContext } from "../context/CalculationContext";
 
 export default function DaySelection() {
-	const [activeDay, setActiveDay] = useState(2);
-	const { dayTypes } = useContext(CalculationContext);
+  const [activeDay, setActiveDay] = useState(2);
+  const { dayTypes } = useContext(CalculationContext);
 
-	const changeActiveDay = (id) => {
-		setActiveDay(id);
-		console.log(activeDay);
-	};
+  const changeActiveDay = (id) => {
+    setActiveDay(id);
+    console.log(activeDay);
+    console.log(activeDay);
+    console.log(activeDay);
+  };
 
-	return (
-		<div>
-			<Flex justifyContent="space-around" boxSize={["100%"]} wrap="wrap">
-				{dayTypes.map((day) => (
-					<DaySelectionElement
-						day={day}
-						key={day.id}
-						onClick={() => {
-							changeActiveDay(day.id);
-						}}
-						active={day.id === activeDay}
-					/>
-				))}
-			</Flex>
-		</div>
-	);
+  return (
+    <div>
+      <Flex justifyContent="space-around" boxSize={["100%"]} wrap="wrap">
+        {dayTypes.map((day) => (
+          <DaySelectionElement
+            day={day}
+            key={day.id}
+            onClick={() => {
+              changeActiveDay(day.id);
+            }}
+            active={day.id === activeDay}
+          />
+        ))}
+      </Flex>
+    </div>
+  );
 }
